097c48e9f8bca038ef40d89c0a6bdbb97324fa0f
Daniyal-Shah <daniyalshah7862018@gmail.com>
2021-11-18 22:29:02 +0500

    ES6 Refresher

--- src/index.js
+++ src/index.js
@@ -6,3 +6,18 @@ const element = <h1>Hello world</h1>
 console.log(element);
 
 ReactDOM.render(element, document.getElementById("root"))
+
+class Person{
+    constructor(name)
+    {
+        this.name=name;
+    }
+}
+
+class Teacher{
+    constructor(name,degree)
+    {
+        super(name);
+        this.degree=degree;
+    }
+}
