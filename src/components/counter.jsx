5e45ece1dd47e5e5cb0d4e5b74e37e4795c1626c
Daniyal-Shah <daniyalshah7862018@gmail.com>
2021-11-23 22:40:40 +0500

    decrement button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,26 +1,44 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    console.log("prevProps", prevProps);
+  }
   render() {
-    let classes = "btn mx-2 btn-";
+    let classes = "badge pill-rounded mx-2  bg-";
     classes += this.props.counter.value == 0 ? "warning" : "primary";
 
     return (
-      <div>
-        <span className={classes}>{this.formatCount()}</span>
+      <div className="container">
+        <div className="row">
+          <div className="col-1  d-flex justify-content-center align-items-center">
+            <span className={classes}>{this.formatCount()}</span>
+          </div>
 
-        <button
-          className="btn btn-info btn-sm m-2"
-          onClick={() => this.props.onIncrement(this.props.counter)}
-        >
-          Increment
-        </button>
-        <button
-          className="btn btn-danger btn-sm m-2"
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-        >
-          Delete
-        </button>
+          <div className="col">
+            {/* Increment Button */}
+            <button
+              className="btn btn-secondary btn-sm m-2 text-center"
+              onClick={() => this.props.onIncrement(this.props.counter)}
+            >
+              +
+            </button>
+            {/* Decrement Button */}
+            <button
+              className="btn btn-secondary btn-sm me-2 "
+              onClick={() => this.props.onDecrement(this.props.counter)}
+              disabled={this.props.counter.value === 0 ? "disabled" : ""}
+            >
+              -
+            </button>
+            <button
+              className="btn btn-danger btn-sm m-2"
+              onClick={() => this.props.onDelete(this.props.counter.id)}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
